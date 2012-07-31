2b9a772268d366c891ab49fb7a24715f1e774d4f
Bertrand Fan <bertrand@fan.net>
2012-07-31 01:29:47 -0700

    Remove instagraph, fake the filter instead so we don't need imagemagick

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../instagraph/instagraph.php';
 
     function createImage($hipinteger, $instagrammed = false) {
         
